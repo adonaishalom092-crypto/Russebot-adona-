97cd93fc9d6bdce1bab04c8af182ce11b2731dd1
adonaishalom092-crypto <adonaishalom092@gmail.com>
2026-05-24 20:18:11 +0300

    Update and rename cfg.py to config.py
    Bot

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+DAILY_BONUS = 10
+REFERRAL_BONUS = 250
+MIN_WITHDRAW = 5000
+REFERRALS_REQUIRED = 20
+CANAL_RETRAIT = "@zarabotok_official0"
